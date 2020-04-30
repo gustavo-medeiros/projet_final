f3e0c800504ac08df68129a9bf44b0e1799123b8
MaxHeitz <63711025+MaxHeitz@users.noreply.github.com>
2020-04-30 15:18:55 +0200

    add notes in forms

--- projet_indiv/taskmanager/forms.py
+++ projet_indiv/taskmanager/forms.py
@@ -3,12 +3,15 @@ from django.forms import forms, ModelForm
 from taskmanager.models import Task, Journal
 
 
+# Un form a partir du model d'une tache
 class NewTaskForm(ModelForm):
     class Meta:
-        model= Task
+        model = Task
         fields = '__all__'
 
+
+# Un form a partir du modele d'un journal
 class NewJournalForm(ModelForm):
     class Meta:
-        model= Journal
+        model = Journal
         fields = '__all__'
