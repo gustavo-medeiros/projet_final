124c19f64d6eaec9a0406a311ea49afe64183f7f
MaxHeitz <63711025+MaxHeitz@users.noreply.github.com>
2020-05-04 15:03:14 +0200

    Update comments in views.py

--- projet_indiv/taskmanager/views.py
+++ projet_indiv/taskmanager/views.py
@@ -1,50 +1,53 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
-
-# Create your views here.
 from taskmanager.models import Project, Task, Journal, Status
-
 from taskmanager.forms import NewTaskForm, NewJournalForm
 
 
-# View associee a la liste des projets
+# Create your views here.
+
+
+# View for the list of projects
+
 @login_required
 def projects(request):
     user = request.user
-    list_projects = []  # On affiche uniquement les projets de l'utilisateur connecte
-    infos=[]
-    count=0
+    list_projects = []  # Each cell of list_projects will contains information about a project
+    infos = []  # the info cell of a project
+    count = 0
     for project in Project.objects.filter(members=user):
-        infos.append(list(Project.objects.filter(members=user))[count])
-        infos.append(Task.objects.filter(project=project))
-        infos.append(Task.objects.filter(project=project).order_by("due_date"))
-        list_projects.append(infos)
-        infos=[]
-        count+=1
+        infos.append(
+            list(Project.objects.filter(members=user))[count])  # 1st cell of infos contains the members of the project
+        infos.append(Task.objects.filter(project=project))  # 2nd cell of infos contains the tasks of the project
+        infos.append(Task.objects.filter(project=project).order_by(
+            "due_date"))  # 3rd cell of infos contains the tasks of the project ordered by due_date
+        list_projects.append(infos)  # add the infos cells in the list of projects
+        infos = []
+        count += 1
     return render(request, 'list_projects.html', locals())
 
 
-# View associée a un projet et a la liste des taches
+# View for the display of a project and its details
 @login_required
 def project(request, id_project):
     user = request.user
     project = Project.objects.get(id=id_project)
-    list_tasks = Task.objects.filter(project=project)  # on affiche la la liste des taches du projet
+    list_tasks = Task.objects.filter(project=project)  # List of the tasks of this project
     return render(request, 'project.html', locals())
 
 
-# La view associee a une tache
+# View for the display of a task and its details
 @login_required
 def task(request, id_task):
     user = request.user
     task = Task.objects.get(id=id_task)
     project = Project.objects.get(id=task.project.id)
-    list_journals = Journal.objects.filter(task=task)  # La liste des entrees journal associees a la tache
+    list_journals = Journal.objects.filter(task=task)  # List of the Journal entries of this task
     return render(request, 'task.html', locals())
 
 
-# View associee a la creation de tache
+# View to fill a form to create a task and add it to the database
 @login_required
 def newtask(request):
     # if this is a POST request we need to process the form data
@@ -53,20 +56,15 @@ def newtask(request):
         form = NewTaskForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-
-            form.save() # on sauvegarde
-
+            form.save()
             project = form['project'].value()
-
-            return task(request, form.instance.id) # on affiche la page associee a cette tache
-        else:
+            return task(request, form.instance.id)  # if the form is valid, save it in database and display the new task
+        else:  # if not, initialize the form and display the form
             form = NewTaskForm()
             list_projects = Project.objects.all()
             list_users = User.objects.all()
             list_status = Status.objects.all()
-
-        return render(request, 'newtask.html', locals())
-
+            return render(request, 'newtask.html', locals())
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -78,7 +76,7 @@ def newtask(request):
     return render(request, 'newtask.html', locals())
 
 
-# VIew associee a l'update d'une tache
+# View to update a task
 @login_required
 def updatetask(request, id_task):
     utask = Task.objects.get(id=id_task)
@@ -90,7 +88,7 @@ def updatetask(request, id_task):
         if form.is_valid():
             # process the data in form.cleaned_data as required
 
-            # On recupere els informations du form
+            # Get the data from the form
             utask.project = form.cleaned_data['project']
             utask.name = form.cleaned_data['name']
             utask.description = form.cleaned_data['description']
@@ -99,11 +97,11 @@ def updatetask(request, id_task):
             utask.start_date = form.cleaned_data['start_date']
             utask.priority = form.cleaned_data['priority']
             utask.status = form.cleaned_data['status']
-            utask.save()  # on modifie l'entree dans la database
+            utask.save()  # Update data in database
 
             project = form['project'].value()
 
-            return task(request, utask.id)  # On redirige vers la tache
+            return task(request, utask.id)  # Display the updated task
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -115,7 +113,7 @@ def updatetask(request, id_task):
     return render(request, 'updatetask.html', locals())
 
 
-# View associee a l'entree d'un journal
+# View to add a new journal entry
 @login_required
 def newjournal(request, id_task):
     jtask = Task.objects.get(id=id_task)
@@ -126,10 +124,9 @@ def newjournal(request, id_task):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
+            # save
             # redirect to a new URL:
             form.save()
-
             return task(request, jtask.id)
         else:
             form = NewJournalForm()
@@ -138,7 +135,6 @@ def newjournal(request, id_task):
             list_status = Status.objects.all()
 
         return render(request, 'newjournal.html', locals())
-
 
     # if a GET (or any other method) we'll create a blank form
     else:
